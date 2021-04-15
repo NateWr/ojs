4a271c55038d9340563779f0d43658d95cedb9f8
Nate Wright <natew@notthisway.com>
2021-04-15 15:11:33 +0100

    Add Map facade

---
+++ classes/facade/Map.inc.php
@@ -0,0 +1,22 @@
+<?php
+
+/**
+ * @file clases/facade/Map.inc.php
+ *
+ * Copyright (c) 2014-2020 Simon Fraser University
+ * Copyright (c) 2000-2020 John Willinsky
+ * Distributed under the GNU GPL v3. For full terms see the file docs/COPYING.
+ *
+ * @class Map
+ *
+ * @brief This facade provides access to all of the map classes in this application
+ */
+
+namespace APP\Facade;
+
+use PKP\Facade\Map as PKPMap;
+
+class Map extends PKPMap
+{
+    //
+}
